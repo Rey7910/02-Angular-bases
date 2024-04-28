1db0669f5442068b4cc0f670925a8d4fb68c9715
Rey7910 <rtoledoleguizamon@gmail.com>
2024-04-28 16:48:47 -0500

    finish modules lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
+
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { ListComponent } from './heroes/list/list.component';
 import { HeroComponent } from './heroes/hero/hero.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     ListComponent,
     HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
