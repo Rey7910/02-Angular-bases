7923942473e690f957b00a8738b5e25e78c88a15
Rey7910 <rtoledoleguizamon@gmail.com>
2024-04-28 14:32:31 -0500

    Finish hero component and directories lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { ListComponent } from './heroes/list/list.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    ListComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
